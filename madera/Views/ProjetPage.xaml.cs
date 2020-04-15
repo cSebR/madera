fcca33d095f36baf280ab53ea139fc532eb0aa27
seb <caroen.sebastien@gmail.com>
2020-04-15 13:13:26 +0200

    ajout image projet

--- madera/Views/ProjetPage.xaml.cs
+++ madera/Views/ProjetPage.xaml.cs
@@ -38,37 +38,29 @@ namespace madera.Views
 
             var item = "client1";
 
-            // Popup choi du client
+            // Popup choix du client
             var client = await DisplayActionSheet("Selectionner un client","Cancel", "OK", item);
             Debug.WriteLine("Client: " + client);
 
-            
-                var client2 = await DisplayActionSheet("Selectionner un client", "Cancel", "OK", item);
-                Debug.WriteLine("Client: " + client);
 
-                //Popup Nom du projet
-                string result = await DisplayPromptAsync("Entrer le nom du projet", "Nom du projet");
-                Debug.WriteLine("Answer: " + result);
+            //Popup Nom du projet
+            string result = await DisplayPromptAsync("Entrer le nom du projet", "Nom du projet");
+            Debug.WriteLine("Answer: " + result);
 
 
-                //Enregistrement en base
-                if (!string.IsNullOrWhiteSpace(result))
+            //Enregistrement en base
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                await App.Database.SaveProjetAsync(new ProjetModel
                 {
-                    //DateTime now = DateTime.Now;
-                    var date1 = DateTime.Now.ToString("dd-MM-yyyy");
+                    Nom = result,
+                    Date = DateTime.Now.ToString("dd-MM-yyyy"),
+                    Ref = result + 1,
+                    Created_by = 1
+                });
 
-                    await App.Database.SaveProjetAsync(new ProjetModel
-                    {
-                        Nom = result,
-                        Date = date1,
-                        Ref = result + 1,
-                        Created_by = 1
-                    });
-
-                    //Debug.WriteLine("Date : " + now); 
-
-                    await Navigation.PushAsync(new ProjetList());
-                }
+                await Navigation.PushAsync(new ProjetList());
+            }
             
         }
 
@@ -77,6 +69,5 @@ namespace madera.Views
             base.OnAppearing();
             await Navigation.PushAsync(new ProjetList());
         }
-
     }
 }
