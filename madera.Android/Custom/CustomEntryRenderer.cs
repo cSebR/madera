680585395f3883052d59b217ce87befabf2b1493
FluryWaana <contact@apicoulet.com>
2020-05-02 09:20:08 +0200

    Correction Button, Input -> Login View

---
+++ madera.Android/Custom/CustomEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Text;
+using madera.Custom;
+using madera.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryWoBorder), typeof(CustomEntryRenderer))]
+namespace madera.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.Transparent);
+                this.Control.SetBackgroundDrawable(gd);
+                this.Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+                Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.White));
+            }
+        }
+    }
+}
