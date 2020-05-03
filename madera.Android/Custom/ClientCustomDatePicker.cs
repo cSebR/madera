6d9646fcda38af5b0636deada0431ee6ce807470
FluryWaana <contact@apicoulet.com>
2020-05-03 22:40:18 +0200

    Ajout: Custom Entry, DatePicker : Android

---
+++ madera.Android/Custom/ClientCustomDatePicker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using madera.Custom;
+using madera.Droid.Custom;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DatePickerClient), typeof(ClientCustomDatePicker))]
+namespace madera.Droid.Custom
+{
+    public class ClientCustomDatePicker : DatePickerRenderer
+    {
+        public ClientCustomDatePicker(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.White);
+                gd.SetCornerRadius(30);
+                Control.Background = gd;
+                Control.SetPadding(20, 5, 20, 5);
+                Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+                Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.Violet));
+            }
+        }
+    }
+}
