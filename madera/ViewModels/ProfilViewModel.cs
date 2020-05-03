0511d49ecafcec58f4191b383e8ea87ca232a796
FluryWaana <contact@apicoulet.com>
2020-05-03 18:36:17 +0200

    [FEATURE] Profil Utilisateur

--- madera/ViewModels/ProfilViewModel.cs
+++ madera/ViewModels/ProfilViewModel.cs
@@ -1,14 +1,40 @@
-﻿using System;
+﻿using madera.Models;
+using madera.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace madera.ViewModels
 {
     public class ProfilViewModel : BaseViewModel
     {
+        /**
+         * Service d'intérrogation API
+         */
+        private readonly AuthService _serviceAuth = new AuthService();
+
         public ProfilViewModel()
         {
             Title = "Profil";
+            Task<Utilisateur> task = _serviceAuth.GetUtilisateurAsync();
+            task.Wait();
+            Utilisateur = task.Result;
+        }
+
+        private Utilisateur _utilisateur;
+        public Utilisateur Utilisateur
+        {
+            get
+            {
+                return _utilisateur;
+            }
+
+            set
+            {
+                _utilisateur = value;
+                OnPropertyChanged();
+            }
         }
     }
 }
